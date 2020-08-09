5b4f7b11ddbde92f3b1b48101e0a4c2803e78c3e
YukiLeafX <yukileafx@gmail.com>
2020-08-09 20:55:44 +0900

    ヘッダーを別ファイルから読み込む

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="app">
-    <nav id="header">
-      <div>
-        <a href="/" class="home">
-          <img src="/favicon.ico">
-          <span>アジ鯖公式ホームページ</span>
-        </a>
-      </div>
-    </nav>
-
+    <AppHeader />
     <p>準備中です...</p>
     <p>Discord: <a href="https://discord.gg/seheC2W">https://discord.gg/seheC2W</a></p>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  components: {
+    AppHeader
+  }
+}
 </script>
 
 <style lang="scss">
@@ -30,35 +29,5 @@ body {
 #app {
   height: 100%;
   width: 100%;
-}
-
-#header {
-  padding: 1.2em;
-  display: flex;
-
-  background-color: #15a1bd;
-
-  .home {
-    padding: 0.8rem;
-    display: flex;
-
-    align-items: center;
-    font-size: 1.5rem;
-    text-decoration: none;
-    color: white;
-
-    img {
-      height: 48px;
-      width: 48px;
-    }
-
-    span {
-      margin-left: 0.5em;
-    }
-
-    &:hover {
-      background: rgba(255, 255, 255, 0.1);
-    }
-  }
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav>
+    <div>
+      <a href="/" class="home">
+        <img src="/favicon.ico">
+        <span>アジ鯖公式ホームページ</span>
+      </a>
+    </div>
+  </nav>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="scss">
+nav {
+  padding: 1.2rem;
+  display: flex;
+
+  background-color: #15a1bd;
+}
+
+.home {
+  padding: 0.8rem;
+  display: flex;
+
+  align-items: center;
+  text-decoration: none;
+  color: white;
+
+  img {
+    height: 3rem;
+  }
+
+  span {
+    margin-left: 0.8rem;
+
+    font-size: 1.5rem;
+  }
+
+  &:hover {
+    background: rgba(255, 255, 255, 0.1);
+  }
+}
+</style>
