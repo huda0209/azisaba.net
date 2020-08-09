22b2781aa9f0ffc38fa6380bbeb259d397ec31b2
YukiLeafX <yukileafx@gmail.com>
2020-08-10 08:19:18 +0900

    メニューの追加

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
   <v-app>
     <v-app-bar app dark color="#144552">
-      <v-img alt="logo" class="shrink mr-3" contain src="/favicon.ico" transition="scale-transition" width="48" />
-      <v-toolbar-title>アジ鯖公式ホームページ</v-toolbar-title>
+      <v-img alt="logo" class="shrink" contain src="/favicon.ico" transition="scale-transition" width="48" />
+      <v-toolbar-title class="ml-3">アジ鯖</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
       <v-btn href="https://azisaba.buycraft.net" target="_blank" text>
         <v-icon>mdi-heart</v-icon>
-        <span class="hidden-sm-and-down ml-2">寄付する</span>
+        <span class="ml-2">寄付</span>
       </v-btn>
+
+      <v-menu transition="slide-y-transition" :close-on-content-click="false" offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn v-on="on" v-bind="attrs" icon>
+            <v-icon>mdi-open-in-app</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list class="pa-2">
+          <v-subheader>開く</v-subheader>
+          <v-list-item v-for="(item, i) in appItems" :key="`app-${i}`" :href="item.url" target="_blank">
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
 
       <v-menu transition="slide-y-transition" :close-on-content-click="false" offset-y>
         <template v-slot:activator="{ on, attrs }">
@@ -52,6 +70,10 @@
     },
 
     data: () => ({
+      appItems: [
+        { title: "リソースパック", icon: "mdi-zip-box", url: "https://packs.azisaba.net" },
+        { title: "ステータス", icon: "mdi-pulse", url: "https://status.azisaba.net" },
+      ],
       socialItems: [
         { title: "Discord", icon: "mdi-discord", url: "https://discord.gg/seheC2W" },
         { title: "Twitter", icon: "mdi-twitter", url: "https://twitter.com/AzisabaNetwork" },
