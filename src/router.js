ae5019a70d24399869d6e9c3cbde6afc97d768c8
YukiLeafX <yukileafx@gmail.com>
2020-08-14 19:49:00 +0900

    リダイレクトの追加

--- src/router.js
+++ src/router.js
@@ -11,6 +11,29 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/discord',
+      beforeEnter: () => {
+        window.location = 'https://discord.gg/seheC2W'
+      }
+    },
+    {
+      path: '/donate',
+      beforeEnter: () => {
+        window.location = 'https://azisaba.buycraft.net'
+      }
+    },
+    {
+      path: '/vote',
+      beforeEnter: () => {
+        let voteUrls = [
+          'https://minecraft.jp/servers/59b7c551a9b0bd23dc00834d/vote',
+          'https://monocraft.net/servers/xWBVrf1nqB2P0LxlMm2v/vote'
+        ]
+        let voteUrl = voteUrls[Math.floor(Math.random() * voteUrls.length)]
+        window.location = voteUrl
+      }
     }
   ]
 })
