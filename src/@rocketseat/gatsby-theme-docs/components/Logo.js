3cf505fd5b08deb7076f4ae409b627985dced84d
YukiLeafX <yukileafx@gmail.com>
2020-10-25 01:46:31 +0900

    ロゴを作る

--- src/@rocketseat/gatsby-theme-docs/components/Logo.js
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -2,11 +2,25 @@ import React from 'react';
 
 export default function Logo(props) {
   return (
-    <img
-      width={180} height={55}
-      src="https://i.azisaba.net/logo.webp"
-      alt="Logo"
-      {...props}
-    />
+    <div style={{
+      width: '180px',
+      height: '34px',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }} {...props}>
+      <img width={34} src="/favicon.png" alt="Favicon" />
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: '8px',
+      }}>
+        <span style={{
+          color: '#000000',
+          fontSize: '1.4em',
+        }}>アジ鯖</span>
+      </div>
+    </div>
   );
 }
