76c614afa4769b0f23f261c6e078a76b6d988c99
YukiLeafX <yukileafx@gmail.com>
2020-11-03 01:49:21 +0000

    ロゴの最適化

--- src/@rocketseat/gatsby-theme-docs/components/Logo.js
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -3,24 +3,18 @@ import React from 'react';
 export default function Logo(props) {
   return (
     <div style={{
-      width: '180px',
-      height: '34px',
+      minWidth: '180px',
+      minHeight: '34px',
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center',
     }} {...props}>
-      <img src="https://res.cloudinary.com/ddhk1wfbi/image/upload/c_fit,f_auto,w_34/v1604359828/1533483330415_b4g00f.jpg" alt="アジ鯖のロゴ部分のアイコン" />
-      <div style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
+      <img src="https://res.cloudinary.com/ddhk1wfbi/image/upload/c_fit,f_auto,w_48/v1604359828/1533483330415_b4g00f.jpg" alt="アジ鯖のロゴ部分のアイコン" />
+      <span style={{
+        color: '#000000',
+        fontSize: '1.4em',
         marginLeft: '8px',
-      }}>
-        <span style={{
-          color: '#000000',
-          fontSize: '1.4em',
-        }}>アジ鯖</span>
-      </div>
+      }}>アジ鯖</span>
     </div>
   );
 }
