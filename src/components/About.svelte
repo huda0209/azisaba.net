3e1497a9f940c7632d32a8e53ec291e64e90fd46
YukiLeafX <yukileafx@gmail.com>
2021-01-06 18:10:29 +0900

    説明を読みやすくする

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,48 +1,49 @@
 <script>
+    import { onMount } from "svelte";
+
     import {
         Grid,
         Row,
         Column,
         ContentSwitcher,
         Switch,
+        AspectRatio,
     } from "carbon-components-svelte";
 
-    const contents = [
-        {
-            title: "アジ鯖って何？",
-            content:
-                "アジ鯖とは、鯖主のアジ氏とネット上で集まった有志による数名のチームで立ち上げられたJava版マイクラサーバーである。ちなみに、数年前に存在した同じ名前である「鯵鯖」というサーバーとは全くの別物である。",
-        },
-        {
-            title: "アジ鯖、たのしい。",
-            content:
-                "アジ鯖の中には複数のサーバーがあり、各サーバーごとに管理が別れています。そのため、多種多様な世界観のサーバーがすべて揃っています！",
-        },
-    ];
-    let selectedIndex = 0;
+    let selectedIndex;
+
+    let contents = [];
+    onMount(async () => {
+        const res = await fetch("/data/about.json");
+        contents = await res.json();
+    });
+
+    $: selected = contents[selectedIndex];
 </script>
 
-<Grid style="padding-top: 1rem; padding-bottom: 1rem;">
-    <Row>
-        <Column sm={4}>
-            <ContentSwitcher bind:selectedIndex>
-                {#each contents as content}
-                    <Switch text={content.title} />
-                {/each}
-            </ContentSwitcher>
-        </Column>
-    </Row>
-</Grid>
+<style>
+    p {
+        font-size: 1.7rem;
+    }
+</style>
 
-<Grid style="padding-top: 3rem; padding-bottom: 3rem;">
-    <Row>
-        <Column md={{ span: 2, offset: 1 }}>
-            <p style="font-size: 1.7rem; margin-bottom: 4rem;">
-                {contents[selectedIndex].title}
-            </p>
-        </Column>
-        <Column md={4}>
-            <p style="font-size: 1.7rem;">{contents[selectedIndex].content}</p>
-        </Column>
-    </Row>
-</Grid>
+<ContentSwitcher bind:selectedIndex>
+    {#each contents as content}
+        <Switch text={content.title} />
+    {/each}
+</ContentSwitcher>
+
+{#if selected}
+    <Grid padding>
+        <Row>
+            <Column md={8} lg={{ span: 4, offset: 3 }}>
+                <AspectRatio ratio="1x1">
+                    <p>{selected.title}</p>
+                </AspectRatio>
+            </Column>
+            <Column md={8} lg={8}>
+                <p>{selected.lines.join('')}</p>
+            </Column>
+        </Row>
+    </Grid>
+{/if}
