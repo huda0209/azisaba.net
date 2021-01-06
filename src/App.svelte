729e58fa2dcc575b389a1834be767a66442da8ed
YukiLeafX <yukileafx@gmail.com>
2021-01-06 18:19:52 +0900

    全てを読み込み、アニメーションをかける

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,39 @@
 <script>
 	import Header from "./components/Header.svelte";
+
 	import Top from "./components/Top.svelte";
 	import About from "./components/About.svelte";
+	import Join from "./components/Join.svelte";
+
+	import { onMount } from "svelte";
+	import { scale, blur, fly } from "svelte/transition";
 
 	import { Content } from "carbon-components-svelte";
+
+	let visible;
+	onMount(() => {
+		visible = true;
+	});
 </script>
 
+<style>
+	section {
+		margin-bottom: 3rem;
+	}
+</style>
+
 <Header />
-<Content style="padding: 0;">
-	<Top />
-	<About />
+
+<Content>
+	{#if visible}
+		<section in:scale={{ start: 1.03 }}>
+			<Top />
+		</section>
+		<section in:blur>
+			<About />
+		</section>
+		<section in:fly={{ y: -20 }}>
+			<Join />
+		</section>
+	{/if}
 </Content>
