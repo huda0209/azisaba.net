527239b921953e40271092f8afaab9950bb1dba8
YukiLeafX <yukileafx@gmail.com>
2021-01-28 19:44:30 +0900

    サイドバーの最適化

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,9 +3,15 @@
         Header,
         SideNav,
         SideNavItems,
+        SideNavMenu,
         SideNavLink,
         SkipToContent,
     } from "carbon-components-svelte";
+    import FavoriteFilled32 from "carbon-icons-svelte/lib/FavoriteFilled32";
+    import Fish32 from "carbon-icons-svelte/lib/Fish32";
+    import Book32 from "carbon-icons-svelte/lib/Book32";
+    import PlayOutline32 from "carbon-icons-svelte/lib/PlayOutline32";
+    import Locked32 from "carbon-icons-svelte/lib/Locked32";
 
     export let segment;
 
@@ -25,25 +31,49 @@
 
 <SideNav bind:isOpen={isSideNavOpen}>
     <SideNavItems>
-        <SideNavLink
-            text="ようこそ"
-            href="/"
-            isSelected={segment === undefined}
-        />
-        <SideNavLink
-            text="アジ鯖とは？"
-            href="/intro"
-            isSelected={segment === "intro"}
-        />
-        <SideNavLink
-            text="ルール"
-            href="/terms"
-            isSelected={segment === "terms"}
-        />
-        <SideNavLink
-            text="今すぐ参加"
-            href="/join"
-            isSelected={segment === "join"}
-        />
+        <SideNavMenu text="はじめに" expanded={true}>
+            <SideNavLink
+                icon={{ render: FavoriteFilled32 }}
+                text="ようこそ"
+                href="/"
+                isSelected={segment === undefined}
+            />
+            <SideNavLink
+                icon={{ render: Fish32 }}
+                text="アジ鯖とは？"
+                href="/intro"
+                isSelected={segment === "intro"}
+            />
+            <SideNavLink
+                icon={{ render: Book32 }}
+                text="ルール"
+                href="/terms"
+                isSelected={segment === "terms"}
+            />
+            <SideNavLink
+                icon={{ render: PlayOutline32 }}
+                text="今すぐ参加"
+                href="/join"
+                isSelected={segment === "join"}
+            />
+        </SideNavMenu>
+        <SideNavMenu text="もっと詳しく" expanded={true}>
+            <SideNavLink icon={{ render: Locked32 }} text="各サーバー紹介" />
+            <SideNavLink icon={{ render: Locked32 }} text="アルバム" />
+            <SideNavLink icon={{ render: Locked32 }} text="運営一覧" />
+            <SideNavLink icon={{ render: Locked32 }} text="スポンサー" />
+        </SideNavMenu>
+        <SideNavMenu text="その他">
+            <SideNavLink
+                text="接続チェック"
+                href="https://mcsrvstat.us/server/azisaba.net"
+                target="_blank"
+            />
+            <SideNavLink
+                text="リソースパックのダウンロード"
+                href="https://packs.azisaba.net"
+                target="_blank"
+            />
+        </SideNavMenu>
     </SideNavItems>
 </SideNav>
